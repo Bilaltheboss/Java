dc8b275ba5a511276b50f417a3b5d0349c827600
Bilaltheboss <137449178+Bilaltheboss@users.noreply.github.com>
2023-06-23 18:38:50 +0500

    Add Four Numbers

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args){
         // Write your code
-        System.out.println("Hello World");
-
+        System.out.println("The Sum Of Four Numbers is");
+        int a = 12;
+        int b = 13;
+        int c = 15;
+        int d = 16;
+        int sum = a + b+c+d;
+        System.out.println(sum);
     }
 }
