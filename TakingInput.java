8355c32c468a15192269273e3d56f9e383481454
Bilaltheboss <137449178+Bilaltheboss@users.noreply.github.com>
2023-06-24 20:05:55 +0500

    complete

--- TakingInput.java
+++ TakingInput.java
@@ -3,12 +3,15 @@ public class TakingInput {
     public static void main(String[] args) {
         System.out.println("Enter Your Name");
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
-        System.out.println("Your name is :" + name);
-        System.out.println("Enter Your Age");
-        int age = sc.nextInt();
-        System.out.println("Your age is : "+ age);
+//        String name = sc.next();
+//        System.out.println("Your name is :" + name);
+//        System.out.println("Enter Your Age");
+//        int age = sc.nextInt();
+        //System.out.println("Your age is : "+ age);
 //        System.out.println(age);
+        String str = sc.nextLine();
+        System.out.println("Your Name Is : " + str);
+
 
 
     }
